e27f1c6dba99fe51d7eeeca276f34ec4f013f600
SrViniih <77179612+SrViniih@users.noreply.github.com>
2026-05-15 15:24:22 +0200

    att

--- dados.js
+++ dados.js
@@ -2,55 +2,28 @@ const produtos = [
     {
         id: 1,
         nome: "Bolo Red Velvet",
-        descricao: "Massa aveludada com recheio tradicional de cream cheese.",
+        descricao: "Masa aterciopelada con relleno tradicional de queso crema.",
         preco: 140.00,
-        categoria: "Bolos Inteiros",
+        categoria: "Pasteles enteros",
         imagem: "https://images.unsplash.com/photo-1586788224331-91284a5148eb?w=500&q=80",
         destaque: true
     },
     {
         id: 2,
-        nome: "Bolo de Pistache e Framboesa",
-        descricao: "Massa de pistache com geleia artesanal de framboesa.",
+        nome: "Pastel de pistacho y frambuesa",
+        descricao: "Pastel de pistacho con mermelada casera de frambuesa.",
         preco: 165.00,
-        categoria: "Bolos Inteiros",
+        categoria: "Pasteles enteros",
         imagem: "https://images.unsplash.com/photo-1535141192574-5d4897c12636?w=500&q=80",
         destaque: true
     },
     {
         id: 3,
-        nome: "Fatia Dois Amores",
-        descricao: "Metade brigadeiro tradicional, metade brigadeiro branco.",
+        nome: "Un trozo de dos amores",
+        descricao: "Mitad brigadeiro tradicional, mitad brigadeiro blanco.",
         preco: 18.00,
-        categoria: "Fatias",
+        categoria: "rebanadas",
         imagem: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=500&q=80",
         destaque: false
     },
-    {
-        id: 4,
-        nome: "Fatia Cenoura com Nutella",
-        descricao: "Massa fofinha de cenoura com cobertura generosa de Nutella.",
-        preco: 20.00,
-        categoria: "Fatias",
-        imagem: "https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=500&q=80",
-        destaque: true
-    },
-    {
-        id: 5,
-        nome: "Caixa de Macarons",
-        descricao: "Caixa com 6 unidades de sabores sortidos.",
-        preco: 45.00,
-        categoria: "Doces Finos",
-        imagem: "https://images.unsplash.com/photo-1569864358642-9d1684040f43?w=500&q=80",
-        destaque: false
-    },
-    {
-        id: 6,
-        nome: "Trufas de Ouro",
-        descricao: "Trufas de chocolate belga pintadas à mão com pó dourado.",
-        preco: 8.00,
-        categoria: "Doces Finos",
-        imagem: "https://images.unsplash.com/photo-1548883354-7622d03aca27?w=500&q=80",
-        destaque: false
-    }
 ];
