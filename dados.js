52d5c5887b51af1b4dab8197672498d99aafb924
SrViniih <77179612+SrViniih@users.noreply.github.com>
2026-05-15 15:33:01 +0200

    att

--- dados.js
+++ dados.js
@@ -3,7 +3,7 @@ const produtos = [
         id: 1,
         nome: "Bolo Red Velvet",
         descricao: "Masa aterciopelada con relleno tradicional de queso crema.",
-        preco: 140.00,
+        preco: 50.00,
         categoria: "Pasteles enteros",
         imagem: "https://images.unsplash.com/photo-1586788224331-91284a5148eb?w=500&q=80",
         destaque: true
@@ -12,7 +12,7 @@ const produtos = [
         id: 2,
         nome: "Pastel de pistacho y frambuesa",
         descricao: "Pastel de pistacho con mermelada casera de frambuesa.",
-        preco: 165.00,
+        preco: 50.00,
         categoria: "Pasteles enteros",
         imagem: "https://images.unsplash.com/photo-1535141192574-5d4897c12636?w=500&q=80",
         destaque: true
@@ -21,7 +21,7 @@ const produtos = [
         id: 3,
         nome: "Un trozo de dos amores",
         descricao: "Mitad brigadeiro tradicional, mitad brigadeiro blanco.",
-        preco: 18.00,
+        preco: 50.00,
         categoria: "rebanadas",
         imagem: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=500&q=80",
         destaque: false
